4f71c7115716f23c84f35d9bffe735266ec5d950
Valentin Heiserer <valentin@heiserer.de>
2024-02-02 20:10:07 +0100

    added folder structure

---
+++ webapp/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        rtsp_url = request.form['rtsp_url']
+        output_path = request.form['output_path']
+
+        # Call your Python script using a subprocess
+        subprocess.run(['python', 'capture_image.py', rtsp_url, output_path])
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
