8ffe5d633b5f9a57894f4691b9fc657cfc9a4dc4
Valentin Heiserer <valentin@heiserer.de>
2024-02-02 22:16:31 +0100

    changes

--- script/createTimelaps.py
+++ script/createTimelaps.py
@@ -1,20 +1,23 @@
 import cv2
 import os
 from datetime import datetime
+import sys
 
-def create_video(images_folder, output_video_path, fps=60, x=1920, y=1080):
+def create_video(images_folder, output_video_path, fps=1, x=1920, y=1080):
     # Get a list of all image files in the folder
     image_files = [f for f in os.listdir(images_folder) if f.endswith(".jpg")]
 
     # Initialize the video writer
-    # Initialize the video writer
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    video_writer = cv2.VideoWriter(output_video_path, fourcc, fps, (x, y))
-
+    timestamp = datetime.now().strftime("%Y_%m_%d_%H_%M")
+    output_video_path_full = os.path.join(output_video_path, f"timelapse_{timestamp}.mp4")
+    os.makedirs(os.path.dirname(output_video_path_full), exist_ok=True)
+    video_writer = cv2.VideoWriter(output_video_path_full, fourcc, fps, (x, y))
 
     # Loop through all image files
     for image_file in image_files:
         image_path = os.path.join(images_folder, image_file)
+        print(image_path)
 
         # Read the image
         image = cv2.imread(image_path)
@@ -23,10 +26,21 @@ def create_video(images_folder, output_video_path, fps=60, x=1920, y=1080):
         if image is not None:
             # Resize the image if needed
             image = cv2.resize(image, (x, y))  # Uncomment and adjust if necessary
-
             # Write the image to the video
             video_writer.write(image)
 
     # Release the video writer
     video_writer.release()
-    
+    print(f"Video saved at {os.path.abspath(output_video_path_full)}")
+
+if __name__ == "__main__":
+    # Check if the command line argument for the images folder is provided
+    if len(sys.argv) < 2:
+        print("Usage: python createTimelaps.py <images_folder>")
+        sys.exit(1)
+
+    # Get the video folder from the command line argument
+    video_folder = sys.argv[1]
+
+    # Save an image from the RTSP stream
+    create_video('images/', video_folder)
--- webapp/app.py
+++ webapp/app.py
@@ -1,18 +1,40 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, send_file
 import subprocess
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
+
+def get_latest_video():
+    video_folder = os.path.join(os.getcwd(), 'webapp/static')
+    videos = [f for f in os.listdir(video_folder) if f.endswith('.mp4')]
+    if videos:
+        # latest_video = max(videos, key=os.path.getctime)
+        return os.path.join(video_folder, videos[6])
+    else:
+        return None
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        rtsp_url = request.form['rtsp_url']
-        output_path = '/app/images/'  # Use the shared volume path
-
         # Call the timelapse script using a subprocess
-        subprocess.run(['python', '/app/timelapse_script.py', rtsp_url, output_path])
+        subprocess.run(['python', 'script/createTimelaps.py', 'webapp/static/'])
+        return redirect(url_for('index'))
 
-    return render_template('index.html')
+
+    # Get the latest video filename
+    latest_video = get_latest_video()
+
+    return render_template('index.html', latest_video=latest_video)
+
+@app.route('/video')
+def video():
+    latest_video = get_latest_video()
+    print("get video " + latest_video)
+    if latest_video:
+        return send_file(latest_video, as_attachment=True)
+    else:
+        return "No video available."
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
