d99d547754a6394a5fa0c89d497063f4945a4323
Valentin Heiserer <valentin@heiserer.de>
2024-02-04 14:16:46 +0100

    changes

--- webapp/app.py
+++ webapp/app.py
@@ -54,4 +54,4 @@ def create_timelapse():
     return jsonify({'message': 'Timelapse creation started.'}), 200
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True, use_reloader=False)
